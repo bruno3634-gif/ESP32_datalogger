2d2563341ce3944294bacb6c99adeb7691bcf4a9
bruno3634-gif <82599521+bruno3634-gif@users.noreply.github.com>
2024-10-16 17:54:13 +0100

    Initial code
    GPS readings
    ESPNOW callbacks
    SD card mouting

---
+++ lib/ESP32_SSD1331-master/ESP32_SSD1331-master/README.md
@@ -0,0 +1,59 @@
+# ESP32_SSD1331
+This is Arduino core for the ESP32 library ( SPI ).  
+Beta ver 1.4
+HSPI only.  
+  
+(example)  
+SCLK-----GPIO #14  
+MOSI-----GPIO #13  
+MISO-----NONE  
+CS-------GPIO #15  
+D/C------GPIO #21  
+RST------GPIO #4  
+  
+Use SPI.h library  
+  
+# Change log
+(1.4)  
+Added function to display 8X8 dot Japanese kanji font.  
+- SSD1331_8x8_Font_DisplayOut
+  
+(1.3)  
+Since delay microseconds can not be used in ESP32 multitasking, we modified it to delay (1).  
+In addition, another argument of the following function is added.  
+- Scroller_8x16_RtoL4line  
+  
+(1.2)  
+Add the following function.  
+- Scroller_8x16_RtoL4line  
+  
+(1.1)  
+Corrected scroll function of electric bulletin board.  
+- Scroller_8x16_RtoL  
+  
+【更新履歴】(Japanese)  
+(1.4)  
+８×８ドットの美咲フォントを表示させるための関数を追加。  
+- SSD1331_8x8_Font_DisplayOut
+  
+(1.3)  
+ESP32 のマルチタスクでは delayMicroseconds が使えない為、delay(1)に修正しました。  
+その他、以下の関数の別引数を追加  
+- Scroller_8x16_RtoL4line  
+  
+(1.2)  
+以下の関数を追加しました。
+- Scroller_8x16_RtoL4line  
+  
+(1.1)  
+電光掲示板風スクロール関数を修正しました。  
+- Scroller_8x16_RtoL  
+  
+  
+The MIT License (MIT)  
+  
+MITライセンス  
+Copyright (c) 2017 Mgo-tec  
+  
+My Blog:  
+https://www.mgo-tec.com
---
+++ src/main.cpp
@@ -0,0 +1,214 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1331.h>
+#include <SoftwareSerial.h>
+#include <TinyGPSPlus.h> // Adicionando a biblioteca TinyGPS++
+#include <SD.h>
+#include <FS.h>
+
+
+#define CS_PIN 5
+#define MOSI_PIN 23
+#define MISO_PIN 19
+#define SCK_PIN 18
+
+const uint16_t OLED_Color_Black = 0x0000;
+const uint16_t OLED_Color_Blue = 0x001F;
+const uint16_t OLED_Color_Red = 0xF800;
+const uint16_t OLED_Color_Green = 0x07E0;
+const uint16_t OLED_Color_Cyan = 0x07FF;
+const uint16_t OLED_Color_Magenta = 0xF81F;
+const uint16_t OLED_Color_Yellow = 0xFFE0;
+const uint16_t OLED_Color_White = 0xFFFF;
+
+// As cores que realmente queremos usar
+uint16_t OLED_Text_Color = OLED_Color_Yellow;
+uint16_t OLED_Backround_Color = OLED_Color_Black;
+
+const uint8_t OLED_pin_scl_sck = 14;
+const uint8_t OLED_pin_sda_mosi = 13;
+const uint8_t OLED_pin_cs_ss = 15;
+const uint8_t OLED_pin_res_rst = 4;
+const uint8_t OLED_pin_dc_rs = 16;
+
+Adafruit_SSD1331 display = Adafruit_SSD1331(OLED_pin_cs_ss, OLED_pin_dc_rs, OLED_pin_sda_mosi, OLED_pin_scl_sck, OLED_pin_res_rst);
+
+unsigned long update_display = 0;
+
+
+
+int incomingsensor;
+int incomingdistance;
+float incomingbat;
+
+String success;
+
+uint8_t BROADCST_ADDRESS[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+typedef struct struct_dados
+{
+    int distance;
+    int sensor;
+    float position;
+} struct_message;
+
+struct_dados incoming_readings;
+
+#define TFT_GRAY 0xBDF7
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+    if (status == 0)
+    {
+        success = "Delivery Success :)";
+    }
+    else
+    {
+        success = "Delivery Fail :(";
+    }
+}
+
+struct_message incomingReadings;
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
+    incomingsensor = incomingReadings.sensor;
+    Serial.println(incomingsensor);
+    incomingdistance = incomingReadings.distance;
+    Serial.println(incomingdistance);
+    incomingbat = incomingReadings.position;
+    Serial.println(incomingbat);
+}
+
+esp_now_peer_info peer_info[3]; // estrutura para guardar dados dos outros ESP
+int count = 0;
+
+SoftwareSerial Serial_software(18, 19); // Pinos para comunicação serial
+TinyGPSPlus gps; // Inicializa o objeto TinyGPS++
+
+int satellites = 0;
+float decimalLatitude = 0.0;
+float decimalLongitude = 0.0;
+bool fix = false;
+
+void setup()
+{
+    update_display = millis();
+    Serial.begin(9600);
+    WiFi.mode(WIFI_STA);
+    Serial_software.begin(9600);
+    display.begin();
+    display.setFont();
+    display.fillScreen(OLED_Backround_Color);
+    display.setTextColor(OLED_Text_Color);
+    display.setTextSize(1);
+
+    /* Tenta inicializar o espnow */
+    if (esp_now_init())
+    {
+        Serial.println("Não foi possível iniciar");
+        return;
+    }
+    else
+    {
+        Serial.println("Iniciado com sucesso");
+    }
+
+    /* Callback para enviar */
+    esp_now_register_send_cb(OnDataSent);
+
+    /* Callback para receber */
+    esp_now_register_recv_cb(OnDataRecv);
+
+    // Registrar peer
+    memcpy(peer_info[0].peer_addr, BROADCST_ADDRESS, 6);
+    peer_info[0].channel = 0;
+    peer_info[0].encrypt = false;
+
+    // Adiciona peer
+    if (esp_now_add_peer(&peer_info[0]) != ESP_OK)
+    {
+        Serial.println("Falha ao adicionar peer");
+        return;
+    }
+    Serial.println("Setup feito");
+
+
+    SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+    if (!SD.begin(CS_PIN)) {
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    else
+    {
+        Serial.println("Card Mount Success");
+    }
+}
+
+void loop()
+{
+   
+
+    while (Serial_software.available() > 0)
+    {
+        char gpsChar = Serial_software.read();
+        Serial.write(gpsChar); // Ecoa os dados do GPS para depuração
+
+        // Passa o caractere para o objeto TinyGPS++
+        gps.encode(gpsChar);
+
+        // Verifica se uma nova sentença foi recebida e, se sim, obtém os dados
+        if (gps.location.isUpdated())
+        {
+            decimalLatitude = gps.location.lat();
+            decimalLongitude = gps.location.lng();
+            fix = gps.location.isValid();
+            satellites = gps.satellites.value();
+
+            Serial.print("Latitude: ");
+            Serial.println(decimalLatitude, 6);
+            Serial.print("Longitude: ");
+            Serial.println(decimalLongitude, 6);
+            Serial.print("Fix: ");
+            Serial.println(fix ? "Yes" : "No");
+            Serial.print("Satellites: ");
+            Serial.println(satellites);
+        }
+    }
+
+    // Atualiza o display OLED a cada 1 segundo
+    if (millis() >= update_display + 1000)
+    {
+        display.fillScreen(OLED_Backround_Color);
+        display.setCursor(0, 0);
+
+        display.setTextColor(OLED_Color_Blue);
+        display.print("Satellites: ");
+        display.setTextColor(OLED_Color_Yellow);
+        display.println(satellites);
+
+        display.setTextColor(OLED_Color_Blue);
+        display.print("Lat: ");
+        display.setTextColor(OLED_Color_Yellow);
+        display.println(decimalLatitude, 6);
+
+        display.setTextColor(OLED_Color_Blue);
+        display.print("Lon: ");
+        display.setTextColor(OLED_Color_Yellow);
+        display.println(decimalLongitude, 6);
+
+        display.setTextColor(OLED_Color_Blue);
+        display.print("Fix: ");
+        display.setTextColor(fix ? OLED_Color_Green : OLED_Color_Red);
+        display.println(fix ? "Yes" : "No");
+
+        update_display = millis();
+    }
+}
